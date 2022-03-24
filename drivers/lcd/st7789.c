ae11bb5356fdd9ce064dc809da11f14816bd60fa
Lee Lup Yuen <luppy@appkaki.com>
2022-03-24 15:10:15 +0800

    SPI Mode 3 for BL602

--- drivers/lcd/st7789.c
+++ drivers/lcd/st7789.c
@@ -47,9 +47,14 @@
 
 /* Verify that all configuration requirements have been met */
 
-#ifndef CONFIG_LCD_ST7789_SPIMODE
-#  define CONFIG_LCD_ST7789_SPIMODE SPIDEV_MODE0
-#endif
+#ifdef CONFIG_BL602_SPI0
+#  warning Using SPI Mode 3 for ST7789 on BL602
+#  define CONFIG_LCD_ST7789_SPIMODE SPIDEV_MODE3 /* SPI Mode 3: Workaround for BL602 */
+#else
+#  ifndef CONFIG_LCD_ST7789_SPIMODE
+#    define CONFIG_LCD_ST7789_SPIMODE SPIDEV_MODE0
+#  endif /* CONFIG_LCD_ST7789_SPIMODE */
+#endif  /* CONFIG_BL602_SPI0 */
 
 /* SPI frequency */
 
